bd8578d9a36b53dcf74f3d1c889df225f2aff661
armineMak <59016002+armineMak@users.noreply.github.com>
2020-01-17 20:35:37 +0400

    Create newtype.py

---
+++ newtype.py
@@ -0,0 +1,23 @@
+str_chnage = "text"
+# print(type(int(str_chnage)))
+str_chnage = bool(str_chnage)
+print(type(str_chnage))
+
+bool_change = True
+bool_change = str(bool_change)
+print(type(bool_change))
+
+float_change = 5.3
+print(float_change)
+float_change = int(float_change)
+print(float_change)
+print(type(float_change))
+
+int_change = 59
+print(int_change)
+int_change = float(int_change)
+print(type(int_change))
+
+# str_float = "type"
+# str_float = float(str_float)
+# print(type(str_float))
