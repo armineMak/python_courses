bb0e060ec0943bcb8dcd2e95c5222f097da9cd4b
armineMak <arminemakaryan1@gmail.com>
2020-01-20 17:02:12 +0400

    test test

---
+++ homework.py
@@ -0,0 +1,18 @@
+user_name = input("Enter username: ")
+user_name = str(user_name)
+print(user_name)
+print(type(user_name))
+
+user_surname = input("Enter user surname: ")
+user_surname = str(user_surname)
+print(user_surname)
+print(type(user_surname))
+
+user_email = input("Enter user email: ")
+user_email = str(user_email)
+print(user_email)
+print(type(user_email))
+
+var_age = int(input("Enter user age: "))
+print(var_age)
+print(type(var_age))
