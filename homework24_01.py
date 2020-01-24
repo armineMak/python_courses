b75b071817894d8f4840a910a6f4b42dd5f87c56
armineMak <arminemakaryan1@gmail.com>
2020-01-24 18:46:10 +0400

    gfgf

---
+++ homework24_01.py
@@ -0,0 +1,11 @@
+user_name = input("Please enter your name: ")
+print(user_name)
+
+user_age = input("Please enter your age: ")
+print(user_age)
+
+var_age = int(user_age)
+var_years_old = 100 - var_age
+var_year = 2020 + var_years_old
+print("In", var_year, "you will be 100 years old.")
+
