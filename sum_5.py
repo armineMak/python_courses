1a53e1bc958e8d8b7875ad7b41ec313258e6e63c
armineMak <arminemakaryan1@gmail.com>
2020-01-27 15:10:31 +0400

    edit homework

--- sum_5.py
+++ sum_5.py
@@ -1,9 +1,12 @@
-greater_num = 3
-smaller_num = 4
-sum_num = greater_num + smaller_num 
+greater_num = int(input("please input number: "))
+smaller_num = int(input("please input the other number: "))
 
-print("the sum is greater than 5: ", bool(sum_num >= 5))
-print("the sum is smaller than 5: ", bool(sum_num <= 5))
-print("the sum is equal to 5: ", bool(sum_num == 5))
-
+sum_num = greater_num + smaller_num
+print(sum_num)
+var_sum = (sum_num >= 5)
+print("the sum is greater than 5: ", var_sum)
+var_sum = (sum_num <= 5)
+print("the sum is smaller than 5: ", var_sum)
+var_sum = (sum_num == 5)
+print("the sum is equal to 5: ", var_sum)
 
