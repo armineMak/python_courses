7e5ae1acae11656e675c86b1bc376245e06efae3
armineMak <arminemakaryan1@gmail.com>
2020-01-27 11:42:08 +0400

    homework

---
+++ sum_5.py
@@ -0,0 +1,9 @@
+greater_num = 3
+smaller_num = 4
+sum_num = greater_num + smaller_num 
+
+print("the sum is greater than 5: ", bool(sum_num >= 5))
+print("the sum is smaller than 5: ", bool(sum_num <= 5))
+print("the sum is equal to 5: ", bool(sum_num == 5))
+
+
